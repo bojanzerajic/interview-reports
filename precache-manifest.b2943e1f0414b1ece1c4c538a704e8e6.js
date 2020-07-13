edf907a74b46a8d5686a1ce608bbec2db0e1f91b
Zeraja <zerajicbojan@gmail.com>
2020-07-13 11:11:07 +0200

    Updates

---
+++ precache-manifest.b2943e1f0414b1ece1c4c538a704e8e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba838e9949098204c738de9897209fc5",
+    "url": "/interview-reports/index.html"
+  },
+  {
+    "revision": "25389c8e649ad5a0f00f",
+    "url": "/interview-reports/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bfc5772c87469bc31cbb",
+    "url": "/interview-reports/static/css/main.3132535e.chunk.css"
+  },
+  {
+    "revision": "25389c8e649ad5a0f00f",
+    "url": "/interview-reports/static/js/2.552fa101.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/interview-reports/static/js/2.552fa101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc5772c87469bc31cbb",
+    "url": "/interview-reports/static/js/main.3b76d1a5.chunk.js"
+  },
+  {
+    "revision": "a753235b48255fdc5b15",
+    "url": "/interview-reports/static/js/runtime-main.750a141e.js"
+  }
+]);
